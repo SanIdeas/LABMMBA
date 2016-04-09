57178613618eb79ea8c139af039cd31990130107
hrherrer <hernan.herreros.13@sansano.usm.cl>
2016-04-09 17:10:47 -0300

    Se agrego extracto del documento al resultado de la búsqueda
    Al arrojar los resultado se muestra un pequeño fragmento de texto y se
    resaltan las palabras buscadas en amarillo.

--- intranet/models.py
+++ intranet/models.py
@@ -42,14 +42,33 @@ class Document(models.Model):
 				result = True
 				result_owner = True
 				result_content = True
+				extract =  ''
 				for word in words:
 					if(not(word in unidecode(self.owner_name()).lower())):
 						result_owner = False
 				#Se revisa el texto plano.
+				print '------------------------------------'
 				for word in words:
-					if(not(word in open(self.document.url.replace('pdf', 'txt')).read())):
+					text = open(self.document.url.replace('pdf', 'txt')).read().replace(' \n' , ' ').replace('\n', ' ').replace('  ', ' ')
+					if(not(word in text)):
 						result_content = False
+
 				result = result_owner or result_content
+
+				if result_content:
+					word = words[0]
+					initial_index = text.index(word)
+					if initial_index - 60 < 0:
+						initial_index = 0
+					else:
+						initial_index -= 60
+						extract += '...'
+					if initial_index + 200 > len(text):
+						end_index = len(text) - 1
+					else:
+						end_index = initial_index + 200
+					extract += text[initial_index:end_index] + '...'
+
 				if result:
 					break
 			elif (field == self._meta.get_field('date')):
@@ -59,5 +78,7 @@ class Document(models.Model):
 						result = False
 				if result:
 					break
+
 		print 'termina', result
-		return result
+		ret = {'match': result, 'extract': extract}
+		return ret
