4ba633bbcd6ac81c125c039e0a497017d99696ed
Alex Balladares Rojas <aleballadaresr@gmail.com>
2016-10-23 17:18:08 -0300

    Arreglo redirecciones en administracion de usuarios y cambios menores

--- admin/views.py
+++ admin/views.py
@@ -1,38 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
-from django.core.mail import BadHeaderError, get_connection, EmailMultiAlternatives
-from login.models import User, Area, UserManager
+from django.http import HttpResponseRedirect, JsonResponse
+from django.core.mail import get_connection, EmailMultiAlternatives
+from login.models import User
 from intranet.models import Document
-from unidecode import unidecode
 from django.core.urlresolvers import reverse
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.utils.crypto import get_random_string
-from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt # SOLO PARA TESTING
 from django.utils.translation import ugettext as _ # Para traducir un string se usa: _("String a traducir")
-
-
-from intranet.forms import DocumentForm
-from django.db.models import Q, Count
-from unidecode import unidecode
-import os, sys, json, operator
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
-from django.core.files import File 
-from pdfminer.pdfdocument import PDFDocument
-from PyPDF2 import PdfFileWriter, PdfFileReader
-from cStringIO import StringIO
-import unicodedata, tempfile
-from datetime import date, timedelta
-from django.utils import timezone
 from collections import Counter
-from itertools import chain
-#import Levenshtein, random
-from django.db import connection
 from django.conf import settings
 
 
@@ -75,87 +50,100 @@ def filters_selected(list, request, name):
 	return list
 
 def documents(request, search=None):
-	if request.user.is_admin:
-		kwargs = get_filters(request)
-		all_docs = Document.objects.filter(**kwargs)
-		if search is None: #Si no es una busqueda
-			documents = all_docs
+	if request.user.is_authenticated:
+		if request.user.is_admin:
+			kwargs = get_filters(request)
+			all_docs = Document.objects.filter(**kwargs)
+			if search is None: #Si no es una busqueda
+				documents = all_docs
+			else:
+				documents = []
+				high_acc_result = []
+				low_acc_result = []
+				authors = []
+				years = []
+				owners = []
+				categories = []
+				for document in all_docs:
+					result = document.match(search)
+					if result['match']:
+						if result['extract'] != '':
+							setattr(document, 'extract', result['extract'])
+						if result['exact']:
+							high_acc_result.append(document)
+						else:
+							low_acc_result.append(document)
+						authors.append(document.author)
+						years.append(document.date.year)
+						owners.append(document.owner)
+						categories.append(document.category)
+				documents = high_acc_result + low_acc_result
+				authors = filters_selected(Counter(authors).most_common(), request, 'author')
+				years = filters_selected(Counter(years).most_common(), request, 'date')
+				owners = filters_selected(Counter(owners).most_common(), request, 'owner')
+				categories = filters_selected(Counter(categories).most_common(), request, 'category')
+			parameters = {'current_view': 'admin', 'documents': documents}
+			if search is not None:
+				parameters['authors'] = authors
+				parameters['years'] = years
+				parameters['categories'] = categories
+				parameters['owners'] = owners
+				parameters['search'] = search
+			return render(request, 'admin/documents.html', parameters)
 		else:
-			documents = []
-			high_acc_result = []
-			low_acc_result = []
-			authors = []
-			years = []
-			owners = []
-			categories = []
-			for document in all_docs:
-				result = document.match(search)
-				if result['match']:
-					if result['extract'] != '':
-						setattr(document, 'extract', result['extract'])
-					if result['exact']:
-						high_acc_result.append(document)
-					else:
-						low_acc_result.append(document)
-					authors.append(document.author)
-					years.append(document.date.year)
-					owners.append(document.owner)
-					categories.append(document.category)
-			documents = high_acc_result + low_acc_result
-			authors = filters_selected(Counter(authors).most_common(), request, 'author')
-			years = filters_selected(Counter(years).most_common(), request, 'date')
-			owners = filters_selected(Counter(owners).most_common(), request, 'owner')
-			categories = filters_selected(Counter(categories).most_common(), request, 'category')
-		parameters = {'current_view': 'admin', 'documents': documents}
-		if search is not None:
-			parameters['authors'] = authors
-			parameters['years'] = years
-			parameters['categories'] = categories
-			parameters['owners'] = owners
-			parameters['search'] = search
-		return render(request, 'admin/documents.html',parameters)
+			return HttpResponseRedirect(reverse('webpage:home'))
+
 	else:
 		return HttpResponseRedirect(reverse('login'))
 
 
 def users(request, user_id=None, delete=False, activate=False, block=False, unblock=False, profile=False):
-	if request.user.is_authenticated() and request.user.is_admin:
-		if request.method == "GET":
-			if activate:
-				user = User.objects.get(id=user_id)
-				user.is_active = True
-				user.save()
-				return JsonResponse({'error': False})
-			elif delete:
-				User.objects.get(id=user_id).delete()
-				return JsonResponse({'error': False})
-			elif block:
-				user = User.objects.get(id=user_id)
-				user.is_blocked = True
-				user.save()
-				return JsonResponse({'error': False})
-			elif unblock:
-				user = User.objects.get(id=user_id)
-				user.is_blocked = False
-				user.save()
-				return JsonResponse({'error': False})
-			elif profile:
-				profile = User.objects.get(id=user_id)
-				documents = Document.objects.filter(owner=user_id)
-				return render(request, 'admin/profile.html', {'profile_user': profile, 'documents': documents})
-			else:
-				return render(request, 'admin/users.html')
+	if request.user.is_authenticated():
+		if request.user.is_admin:
+			if request.method == "GET":
+				if activate and request.is_ajax():
+					user = User.objects.get(id=user_id)
+					user.is_active = True
+					user.save()
+					return JsonResponse({'error': False})
+				elif delete and request.is_ajax():
+					User.objects.get(id=user_id).delete()
+					return JsonResponse({'error': False})
+				elif block and request.is_ajax():
+					user = User.objects.get(id=user_id)
+					user.is_blocked = True
+					user.save()
+					return JsonResponse({'error': False})
+				elif unblock and request.is_ajax():
+					user = User.objects.get(id=user_id)
+					user.is_blocked = False
+					user.save()
+					return JsonResponse({'error': False})
+				elif profile and request.is_ajax():
+					profile = User.objects.get(id=user_id)
+					documents = Document.objects.filter(owner=user_id)
+					return render(request, 'admin/profile.html', {'profile_user': profile, 'documents': documents})
+				else:
+					return render(request, 'admin/users.html')
 
-		elif request.method == "POST":
-			args = {
-				'not_registered': User.objects.filter(is_registered=False, is_admin=False),
-				'registered': User.objects.filter(is_registered=True, is_blocked=False, is_admin=False),
-				'blocked': User.objects.filter(is_registered=True, is_blocked=True, is_admin=False)
-			}
-			return render(request, 'admin/users_ajax.html', args)
+			elif request.method == "POST" and request.is_ajax():
+				args = {
+					'not_registered': User.objects.filter(is_registered=False, is_admin=False),
+					'registered': User.objects.filter(is_registered=True, is_blocked=False, is_admin=False),
+					'blocked': User.objects.filter(is_registered=True, is_blocked=True, is_admin=False)
+				}
+				return render(request, 'admin/users_ajax.html', args)
+		else:
+			if request.is_ajax():
+				return JsonResponse({'redirect': reverse('webpage:home')})
+			else:
+				return HttpResponseRedirect(reverse('webpage:home'))
 
 	else:
-		return HttpResponseRedirect(reverse('login'))
+		if request.is_ajax():
+			return JsonResponse({'redirect': reverse('login')})
+		else:
+			return HttpResponseRedirect(reverse('login'))
 
 def sendInvitation(request):
 	if request.user.is_authenticated():
@@ -211,4 +199,4 @@ def sendInvitation(request):
 			return HttpResponseRedirect(reverse('webpage:home'))
 
 	else:
-		return HttpResponseRedirect(reverse('webpage:home'))
+		return HttpResponseRedirect(reverse('login'))
