1f56e3d5c0cd0e000e88b41011326e09da3e1954
Ricardo Gamboa <ricardo.gamboa@live.com>
2016-12-13 02:59:12 -0300

    Se ven los primeros graficos

---
+++ admin/templatetags/googleanalytics.py
@@ -0,0 +1,22 @@
+import json
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+from django.conf import settings
+
+
+from django import template
+ 
+register = template.Library()
+ 
+@register.inclusion_tag(settings.ANALYTICS_HTML_DIR , takes_context=True)
+def analytics(context, next = None):
+
+	# The scope for the OAuth2 request.
+	SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+	credentials = ServiceAccountCredentials.from_json_keyfile_name( settings.ANALYTICS_JSON_DIR , SCOPE)
+	ANALYTICS_VIEW_ID = '135953807'
+	 
+	return {
+		'token': credentials.get_access_token().access_token,
+		'view_id': ANALYTICS_VIEW_ID
+	}
